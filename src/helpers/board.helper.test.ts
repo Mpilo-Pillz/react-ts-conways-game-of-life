0d73a19588f8a4ee12d095cda4d55f6f1c7e6caf
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-24 08:19:19 +0200

    testing cell death by under population

--- src/helpers/board.helper.test.ts
+++ src/helpers/board.helper.test.ts
@@ -1,4 +1,8 @@
-import { plotTilesRandomly, resetGrid } from "../helpers/board.helper";
+import {
+  gameLogic,
+  plotTilesRandomly,
+  resetGrid,
+} from "../helpers/board.helper";
 
 test("It should return an array 25 elements", () => {
   expect(resetGrid(25, 35)).toHaveLength(25);
@@ -14,8 +18,22 @@ test("It should return an array with all elements equal 0 representing all dead 
 
 test("It should return an array with the element 1 representing the presence of at least 1 living cell", () => {
   const hasLivingCell = plotTilesRandomly(25, 35).some((row) =>
-    row.some((one) => one === 1)
+    row.some((alive) => alive === 1)
   );
 
   expect(hasLivingCell).toBe(true);
+});
+
+test("Cells should die of under population if they have fewer than 2 neighbors,", () => {
+  const fewerThanTwoNeighbors = jest.fn().mockImplementation(() => [
+    [0, 0, 1],
+    [0, 0, 0],
+    [1, 0, 0],
+  ]);
+
+  expect(gameLogic(fewerThanTwoNeighbors(), 3, 3)).toEqual([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
 });
